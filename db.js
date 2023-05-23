c281357e65c58af806e8a67546468e6a6f7dbd18
Ginger Owen <gingerowen0421@gmail.com>
2023-05-23 10:51:11 -0600

    switched to mysql2, have .env working

--- db.js
+++ db.js
@@ -1,10 +1,20 @@
-var mysql = require('mysql')
+const mysql = require('mysql2');
 
-var connection = mysql.createConnection({
+require('dotenv').config();
+
+let user = process.env.DB_USER;
+let password = process.env.DB_PASSWORD;
+//console log your .env varaible to make sure you are grabbing them from .env file
+//console.log(user, password);
+
+var connection = mysql.createConnection(
+    {
     host: 'localhost',
+    user: user,
+    password: password,
+    // user: 'root',
+    //password: 'Tadgage#2627',
     database: 'booklist_database',
-    user: 'root',
-    password: ''
 });
 
 module.exports = connection;
